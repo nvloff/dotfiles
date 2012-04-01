b5afda76cc5c43d4dab80483955ab1b04b386d8b
Nikolay Vladimirov <nikolay@vladimiroff.com>
2012-04-01 17:23:36 +0300

    add command-t, and update all plugins

--- bash/env.bash
+++ bash/env.bash
@@ -1,5 +1,5 @@
-export PATH=$PATH:$HOME/.local/bin:$HOME/bin
+export PATH=$PATH:$HOME/local/bin:$HOME/bin
 export PATH=$PATH:$HOME/.gems/bin
 export PATH="$HOME/.rbenv/bin:$PATH"
-export GEM_HOME="$HOME/.gems"
-export GEM_PATH="$GEM_HOME"
+#export GEM_HOME="$HOME/.gems"
+#export GEM_PATH="$GEM_HOME"
