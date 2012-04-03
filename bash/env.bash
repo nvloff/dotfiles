f39dd839d701c93ecfca93dc35a2458a8fc7ae32
Nikolay Vladimirov <nikolay@vladimiroff.com>
2012-04-03 09:15:16 +0300

    bash.env: change java run options

--- bash/env.bash
+++ bash/env.bash
@@ -1,5 +1,7 @@
 export PATH=$PATH:$HOME/local/bin:$HOME/bin
 export PATH=$PATH:$HOME/.gems/bin
 export PATH="$HOME/.rbenv/bin:$PATH"
-#export GEM_HOME="$HOME/.gems"
-#export GEM_PATH="$GEM_HOME"
+
+# JRuby x86_64 JVM works only in server mode which
+# has slow startup, so start the i686 java in client mode
+export JAVA_OPTS="-d32 -client"
