bdeb0d0ffd5473eb5f0acad8110cd3cee5f215b0
Nikolai Vladimirov <nikolay@vladimiroff.com>
2012-05-16 14:30:45 +0300

    remove jruby stuff

--- bash/env.bash
+++ bash/env.bash
@@ -1,7 +1,3 @@
 export PATH=$PATH:$HOME/local/bin:$HOME/bin
 export PATH=$PATH:$HOME/.gems/bin
 export PATH="$HOME/.rbenv/bin:$PATH"
-
-# JRuby x86_64 JVM works only in server mode which
-# has slow startup, so start the i686 java in client mode
-export JAVA_OPTS="-d32 -client"
