c5630149edcd6957cc85813901379f3c0ee0fa8f
Nikolai Vladimirov <nikolay@vladimiroff.com>
2014-07-27 12:44:14 +0300

    remove unused bash env

--- bash/env.bash
+++ bash/env.bash
@@ -1,4 +1,2 @@
 export PATH=$PATH:$HOME/local/bin:$HOME/bin
-export PATH=$PATH:$HOME/.gems/bin
-export PATH="$HOME/.rbenv/bin:$PATH"
-export JRUBY_OPTS="--server -Xcompile.invokedynamic=false -J-XX:+TieredCompilation -J-XX:TieredStopAtLevel=1 -J-noverify -J-Xms512m -J-Xmx1024m"
+export PATH=/usr/local/bin:$PATH
