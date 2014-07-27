d802c283917a3eb36075aab713ab87d22ea5acbf
Nikolai Vladimirov <nikolay@vladimiroff.com>
2014-07-27 12:43:39 +0300

    remove unused aluase

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,5 +1,4 @@
 alias g='git'
 __git_complete g __git_main
 alias ll='ls -lsh'
-alias test_env='export CUCUMBER_SUFFIX=_cucumber && export RAILS_ENV="test" && export RACK_ENV="test"'
 alias be='bundle exec'
