bfb867745b1aafa98320969dcdf41be29ac59ce3
Nikolai Vladimirov <nikolay@vladimiroff.com>
2018-05-08 00:38:51 +0300

    aliases/install

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -1,4 +1,6 @@
 alias g='git'
 __git_complete g __git_main
 alias ll='ls -lsh'
-alias be='dotenv bundle exec'
+alias be='bundle exec'
+alias vim='nvim'
+
--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@ shopt -s extglob
 dotfiles_dir="$HOME/rcfiles"
 
 main() {
-	local exclude=".git|.gitignore|.gitmodules|install.sh|.config|.|..|"
+  local exclude=".git|.gitignore|.gitmodules|install.sh|.config|.|..|"
 
 
-	for entry in ./!($exclude)
-	do
-		ln -vnsf $dotfiles_dir/$entry $HOME/$entry
-	done
+  for entry in ./!($exclude)
+  do
+    ln -vnsf $dotfiles_dir/$entry $HOME/$entry
+  done
 }
 
 copy_config() {
 
-	for entry in .config/*
-	do
-		ln -vns $dotfiles_dir/$entry $HOME/$entry
-	done
+  for entry in .config/*
+  do
+    ln -vns $dotfiles_dir/$entry $HOME/$entry
+  done
 
 }
 
