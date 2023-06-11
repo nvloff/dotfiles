b9c85166c8153dd6081e330f486712e8397d4217
Nikolai Vladimirov <127317+nvloff@users.noreply.github.com>
2023-06-11 16:57:36 +0300

    enable spell check

--- nvim/lua/personal/settings.lua
+++ nvim/lua/personal/settings.lua
@@ -107,22 +107,21 @@ opt.inccommand = "nosplit" -- preview incremental substitute
 opt.laststatus = 0
 opt.list = true            -- Show some invisible characters (tabs...
 opt.pumblend = 10          -- Popup blend
-opt.pumheight = 10         -- Maximum number of entries in a popup
+opt.pumheight = 10         -- Maximum number of entries in a pop-up
 opt.scrolloff = 4          -- Lines of context
 opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 opt.shiftround = true      -- Round indent
 opt.shiftwidth = 4         -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true })
-opt.showmode = false       -- Dont show mode since we have a statusline
+opt.showmode = false       -- Don't show mode since we have a statusline
 opt.sidescrolloff = 8      -- Columns of context
 opt.signcolumn = "yes"     -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true       -- Don't ignore case with capitals
 opt.smartindent = true     -- Insert indents automatically
-opt.spelllang = { "en" }
 opt.splitbelow = true      -- Put new windows below current
 opt.splitright = true      -- Put new windows right of current
 opt.tabstop = 4            -- Number of spaces tabs count for
-opt.termguicolors = true   -- True color support
+opt.termguicolors = true   -- True colour support
 opt.timeoutlen = 300
 opt.undofile = true
 opt.undolevels = 10000
@@ -130,3 +129,7 @@ opt.updatetime = 200               -- Save swap file and trigger CursorHold
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5                -- Minimum window width
 opt.wrap = false                   -- Disable line wrap
+
+-- Enable
+opt.spelllang = { "en_gb" }
+opt.spell = true
