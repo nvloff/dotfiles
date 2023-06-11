70b07234a36d9f6f335ff2c9ea7bce31a1c79cc1
Nikolai Vladimirov <127317+nvloff@users.noreply.github.com>
2023-06-11 13:47:56 +0300

    bash: cleanup env

--- bash/env.bash
+++ bash/env.bash
@@ -1,9 +1,6 @@
 export PATH=$PATH:$HOME/local/bin:$HOME/bin
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
-export PATH="~/.rbenv/shims:$PATH"
-export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH="$HOME/.exenv/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
 
 export GOPATH="$HOME/go"
