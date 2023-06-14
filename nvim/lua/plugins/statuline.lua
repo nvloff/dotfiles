e38db55fcd76c48ace090fdf64131f5d34a89262
Nikolai Vladimirov <127317+nvloff@users.noreply.github.com>
2023-06-14 06:41:47 +0300

    nvim changes
    - fix status line
    - comment settings

--- nvim/lua/plugins/statuline.lua
+++ nvim/lua/plugins/statuline.lua
@@ -5,8 +5,8 @@ return {
 		-- See `:help lualine.txt`
 		opts = {
 			options = {
-				icons_enabled = false,
-				theme = 'onedark',
+				icons_enabled = true,
+				theme = 'auto',
 				component_separators = '|',
 				section_separators = '',
 			},
