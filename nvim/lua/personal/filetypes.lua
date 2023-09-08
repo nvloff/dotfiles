64df89f98cd65bed28b8827a78310d9db33fb053
Nikolai Vladimirov <127317+nvloff@users.noreply.github.com>
2023-09-08 07:04:32 +0300

    nvim: tiltfile

--- nvim/lua/personal/filetypes.lua
+++ nvim/lua/personal/filetypes.lua
@@ -1,13 +1,5 @@
-vim.api.nvim_create_autocmd("BufRead", {
-  pattern = "Tiltfile",
-  callback = function ()
-    local buf = vim.api.nvim_get_current_buf()
-    vim.api.nvim_buf_set_option(buf, "filetype", "tiltfile")
-  end
-})
-
 vim.filetype.add({
   pattern = {
-    ['*.[T|t]iltfile.*'] = 'starlark',
+    ['.*/.*[T|t]iltfile.*'] = 'starlark',
   },
 })
