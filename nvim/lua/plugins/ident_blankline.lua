9aec02e178de21c139a34f4e4bc3dd1d770222ab
Nikolai Vladimirov <127317+nvloff@users.noreply.github.com>
2023-10-16 06:22:59 +0300

    nvim: config indent blankline

--- nvim/lua/plugins/ident_blankline.lua
+++ nvim/lua/plugins/ident_blankline.lua
@@ -5,9 +5,6 @@ return {
         -- Enable `lukas-reineke/indent-blankline.nvim`
         -- See `:help indent_blankline.txt`
         main = "ibl",
-        opts = {
-            char = '┊',
-            show_trailing_blankline_indent = false,
-        },
+        opts = {},
     },
 }
