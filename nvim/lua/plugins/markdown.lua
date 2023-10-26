e54b04a2c532a6b5c3298788844ad72861c3f0f0
Nikolai Vladimirov <127317+nvloff@users.noreply.github.com>
2023-10-26 07:42:44 +0300

    nvim: lazy markdown-preview

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 return {
     "iamcco/markdown-preview.nvim",
-    config = function()
-      vim.fn["mkdp#util#install"]()
-    end,
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
 }
