bbd4591e3bdae9088b82f2ce10a50222ce6ba1b2
Nikolai Vladimirov <127317+nvloff@users.noreply.github.com>
2026-03-08 05:25:09 +0200

    clean up ts config

--- nvim/init.lua
+++ nvim/init.lua
@@ -716,8 +716,6 @@ require('lazy').setup({
     build = ':TSUpdate',
     branch = 'main',
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
-      require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
           local buf, filetype = args.buf, args.match
