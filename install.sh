db077c508f8d5a38326cbce7f890e7b90555fdbe
Nikolai Vladimirov <127317+nvloff@users.noreply.github.com>
2026-07-29 08:17:06 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ shopt -s extglob
 # ln -vnsf "$PWD/profile" "$HOME/.profile"
 #
 # ln -vnsf "$PWD/gitconfig" "$HOME/.gitconfig"
+# ln -vnsf "$PWD/gitignore_global" "$HOME/.gitignore_global"
 
 #ln -vnsf "$PWD/nvim" "$HOME/.config/nvim"
 ln -vnsf "$PWD/ghostty" "$HOME/.config/ghostty"
